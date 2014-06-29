12498ff1741787429166cbacd8bfcd2adf8355bf
Jason Hoi Win7 <flashjx1985@gmail.com>
2014-06-29 18:14:59 +0800

    add Facade

---
+++ Facade/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FacadeLibrary;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Facade Pattern\n");
+
+            Facade.Operation1();
+            Facade.Operation2();
+
+            Console.ReadLine();
+
+            /*
+             * Output:
+             * 
+             * Facade Pattern
+             * 
+             * operation 1
+             * Subsystem A, Method A1
+             * Subsystem A, Method A2
+             * Subsystem B, Method B1
+             * 
+             * opeartion 2
+             * Subsystem B, Method B1
+             * Subsystem C, Method C1
+             */
+        }
+    }
+}
---
+++ FacadeLibrary/Library.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeLibrary
+{
+    internal class SubsystemA
+    {
+        internal string A1()
+        {
+            return "Subsystem A, Method A1\n";
+        }
+        internal string A2()
+        {
+            return "Subsystem A, Method A2\n";
+        }
+    }
+
+    internal class SubsystemB
+    {
+        internal string B1()
+        {
+            return "Subsystem B, Method B1\n";
+        }
+    }
+
+    internal class SubsystemC
+    {
+        internal string C1()
+        {
+            return "Subsystem C, Method C1\n";
+        }
+    }
+
+    public static class Facade
+    {
+        static SubsystemA a = new SubsystemA();
+        static SubsystemB b = new SubsystemB();
+        static SubsystemC c = new SubsystemC();
+
+        public static void Operation1()
+        {
+            Console.WriteLine("operation 1\n" +
+                a.A1() +
+                a.A2() +
+                b.B1());
+        }
+
+        public static void Operation2()
+        {
+            Console.WriteLine("operation 2\n" +
+                b.B1() +
+                c.C1());
+        }
+
+        public static void Main()
+        {
+        }
+    }
+}
